03ed25ca6fcf8cb6e183be367c0fdadaacd11805
immrroboto <connor.riley2@gmail.com>
2018-12-13 21:24:29 -0500

    rm extraneous js files, minor copy changes

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ def landing():
 
 @app.route("/2018_city_budget")
 def CityBudget2018():
-    what_this = '''This is an interactive exploration of Richmond City's 2018 budget.  The concentric rings (from inner-most to outer) are agency, cost center, account, then service.'''
+    what_this = '''This is an interactive exploration of Richmond City's 2018 budget.  The concentric rings (from inner-most to outer) are agency, cost center, service, then account.'''
 
     how_to_use = '''Click through the sections of the pie to dive deeper into that particular line item - click the circle in the center to go back up a level. Enjoy! '''
 
-    where_data = '''The data comes from the City of Richmond's <a href="https://data.richmondgov.com/Well-Managed-Government/City-Budget-General-Fund/7nru-hsrx">Data Portal</a> and was converted into a tree structure (of the acyclic fully connected graph nature) in Python before being saved as a JSON object.  You can find the code for preparing this data for web use in the RVA Data Gallery <a href="#">here</a>.'''
+    where_data = '''The data comes from the City of Richmond's <a href="https://data.richmondgov.com/Well-Managed-Government/City-Budget-General-Fund/7nru-hsrx">Data Portal</a> and was converted into a tree structure (of the acyclic fully connected graph nature) in Python before being saved as a JSON object.  You can find the code for preparing this data for web use in the RVA Data Gallery <a href="https://github.com/IMMrRoboto/rva_data_gallery/tree/master/munging_code/2018_rva_budget">here</a>.'''
 
     shoutouts = '''D3.js sunbirst derived from Mike Bostock's <a href="https://beta.observablehq.com/@mbostock/d3-zoomable-sunburst">example</a>. '''
 
